123538cabe67243ab04c90350dd7059ba9918dcd
alphonsezhang <lovestudy@qq.com>
2024-08-05 08:16:41 +0800

    silu

--- src/operators.rs
+++ src/operators.rs
@@ -77,13 +77,14 @@ pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: 
 // y = sigmoid(x) * x * y
 // hint: this is an element-wise operation
 pub fn silu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
-    // let len = y.size();
-    // assert!(len == x.size());
+    let len = y.size();
+    assert!(len == x.size());
 
-    // let _y = unsafe { y.data_mut() };
-    // let _x = x.data();
-
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+    let y_data = unsafe { y.data_mut() };
+    let x_data = x.data();
+    for i in 0..x_data.len()  {
+        y_data[i]=y_data[i] *x_data[i] *1.0 / (1.0 + (-x_data[i]).exp())
+    }
 }
 
 // C = beta * C + alpha * A @ B^T
