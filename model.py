219699a43850487ce1252723fd9cf43a9455c922
alphonsezhang <lovestudy@qq.com>
2024-08-21 18:36:38 +0800

    量化

--- model.py
+++ model.py
@@ -1,15 +1,35 @@
-from transformers import LlamaForCausalLM,LlamaTokenizer
-def generate(parameters):
-    
+import torch
+import os
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig
+# from bitsandbytes import 
 
-model_path = "/models/story/"
+# quantization_config = QuantizationConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.bfloat16
+# )
 
-# 加载预训练的模型和相应的分词器
-# tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-model = LlamaForCausalLM.from_pretrained(
-    model_path,
-    # device_map="auto",  # 如果有GPU，这会自动将模型分配到GPU上
+# 模型路径
+model_id = './models/story/'
+
+# 模型加载，转换
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, # 模型路径
+    local_files_only=True,# 仅从本地加载模型，不从网络下载。
+    torch_dtype=torch.float16, # 型权重的数据类型转换为 float16
+     quantization_config = BitsAndBytesConfig(
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    ),
+    # device_map='auto' # 自动分配设备
 )
-tokenizer = LlamaTokenizer.from_pretrained(model_path)
-input = "Once upon a time"
+
+
+output = "./soulteary/Chinese-Llama-2-7b-4bit"
+if not os.path.exists(output):
+    os.mkdir(output)
+
+model.save_pretrained(output)
+print("done")
 
